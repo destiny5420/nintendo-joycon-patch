e097e223f31247e249cc3a30a3d95e15e2e28a45
SikaPaper <destiny5420@gmail.com>
2019-06-16 23:41:06 +0800

    [Modify] 修正Demo.cs

--- Assets/Scripts/DemoScript.cs
+++ Assets/Scripts/DemoScript.cs
@@ -1,38 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using PaperGmaeStudio.Controller;
 
 public class DemoScript : MonoBehaviour
 {
-    string[] controlls;
-
     void Start()
     {
         NintendoJoyconManager.Init();
         NintendoJoyconManager.RegistCallback(NintendoJoyconManager.NINTENDO_KEY_TYPE.Player01_A, OnClick_A_01);
-        NintendoJoyconManager.RegistCallback(NintendoJoyconManager.NINTENDO_KEY_TYPE.Player01_B, OnClick_B_01);
-        NintendoJoyconManager.RegistCallback(NintendoJoyconManager.NINTENDO_KEY_TYPE.Player01_Y, OnClick_Y_01);
-        NintendoJoyconManager.RegistCallback(NintendoJoyconManager.NINTENDO_KEY_TYPE.Player01_X, OnClick_X_01);
     }
 
     void OnClick_A_01(float v_value)
     {
         Debug.Log("OnClick_A_01 / value: " + v_value);
-    }
-
-    void OnClick_B_01(float v_value)
-    {
-        Debug.Log("OnClick_B_01 / value: " + v_value);
-    }
-
-    void OnClick_Y_01(float v_value)
-    {
-        Debug.Log("OnClick_Y_01 / value: " + v_value);
-    }
-
-    void OnClick_X_01(float v_value)
-    {
-        Debug.Log("OnClick_X_01 / value: " + v_value);
     }
 }
